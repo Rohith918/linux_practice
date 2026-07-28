f0367a8bf6f46485908fada665595dc174a190a4
Rohith918 <nallamdarohith9@gmail.com>
2026-07-28 15:51:17 +0000

    Day 2 of Devops completed till github

---
+++ practice.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wham="$(whoami)"
+
+dt="$(Date)"
+
+echo "$wham is current user and today is $dt" > output.txt
+
+liost=("Linux" "Shell Scripting" "Github")
+
+for i in "${liost[@]}"; do
+	if [3 -gt 2]; then
+		echo "$i is completed">>output.txt
+	else
+		echo "More work is to be Done">>output.txt
+	fi
+done
